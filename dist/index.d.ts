113b2a09f97e9c5e423b2f4120c4bd8a5a466118
Kieve Chua <kievechua@gmail.com>
2017-05-03 11:46:41 +0800

    Fix loading from wrong config

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import { Module } from 'magnet-core/module';
-export default class David extends Module {
-    davidConfig: any;
+export default class MagnetDavid extends Module {
+    readonly moduleName: string;
+    readonly defaultConfig: string;
     setup(): Promise<void>;
     listDependencies(deps: any): void;
 }
